85f2a2a5029a20a64700668a6dccbdc93e05ecc9
Salatrik <philip.salatrik@compuware.com>
2018-11-19 13:12:14 -0500

    Complete fake comments/could use way more work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './comment_detail';
+import faker from 'faker';
+import ApprovalCard from './approval_card';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+	return (	
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+		<div className="ui container comments">
+
+		<ApprovalCard>
+				<div><h4>Warning</h4>Are you sure you want to do this</div>
+		</ApprovalCard>
+
+		<ApprovalCard >
+			<CommentDetail 
+					author="Sam" 
+					timeAgo="Today at 5::15pm" 
+					commentText="Hey hey Hey Hockytown"
+					avatar={faker.image.avatar()} />
+		</ApprovalCard>
+
+		<ApprovalCard >
+			<CommentDetail 
+					author="Alex" 
+					timeAgo="Yesterday ae 3pm" 
+					commentText="Nice! You React well"
+					avatar={faker.image.avatar()}/>
+		</ApprovalCard>
+		
+		<ApprovalCard>			
+			<CommentDetail 
+					author="Jane" 
+					timeAgo="One Week ago at 2:15am" 
+					commentText="What the what?!"
+					avatar={faker.image.avatar()} />
+		</ApprovalCard>
+
+		</div>
+
+			);
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
